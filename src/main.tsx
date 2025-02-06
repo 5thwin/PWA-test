d043c8d4aa6605ebeb6ed3228a7d98bae105f9d6
5thwin <hisy2059@naver.com>
2025-02-06 11:26:09 +0900

    FIX: registerSW 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { registerSW } from 'virtual:pwa-register'; //not find module error 해결: tsconfig.app.json의 include에 node_modules/vite-plugin-pwa/client.d.ts
+
+const updateSW = registerSW({
+	onNeedRefresh() {
+		if (confirm('새 버전이 있습니다! 업데이트하시겠습니까?')) {
+			updateSW(); // 새로운 서비스 워커를 적용하여 즉시 업데이트
+		}
+	},
+	onOfflineReady() {
+		console.log('PWA가 오프라인에서 사용할 준비가 되었습니다.');
+	},
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
